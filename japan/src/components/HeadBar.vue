9e0e6120671bcc1ca42314cd2b82ad57e1176481
unknown <06793@udnprod.local>
2019-09-30 16:36:05 +0800

    japan on test

---
+++ japan/src/components/HeadBar.vue
@@ -0,0 +1,135 @@
+<template>
+  <div class="head-bar-container">
+    <div class="logo-wrapper" @click="sendLogoGA()">
+      <a :href="homePath" target="_blank" name="udn-logo-button">
+        <i class="udn-icon udn-icon-logo"/>
+      </a>
+    </div>
+    <div class="share-button-container">
+      <button class="share-button" name="fb-button" @click="fbShare()">
+        <img :src="require('../assets/fb.png')" alt="fb-icon">
+      </button>
+      <button class="share-button" name="line-button" @click="lineShare()">
+        <img :src="require('../assets/line.png')" alt="line-icon">
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Utils from 'udn-newmedia-utils';
+
+export default {
+  name: 'HeadBar',
+  data() {
+    return {
+      homePath: window.location.origin + '/newmedia/2019/global_energy_transition/'
+    };
+  },
+  computed: {
+    isMob() {
+      return window.innerWidth <= 768 ? true : false;
+    },
+  },
+  methods: {
+    fbShare() {
+      window.FB.ui(
+        {
+          method: 'share',
+          href: this.href,
+        }, 
+        // function(response) {}
+      );
+      window.ga('newmedia.send', {
+        hitType: 'event',
+        eventCategory: 'Share',
+        eventAction: 'click',
+        eventLabel: `[${Utils.detectPlatform()}] [${document.querySelector('title').innerHTML}] [特製fb icon] [Click_FB_top]`,
+      });
+    },
+    lineShare() {
+      if (Utils.detectMob()) {
+        // 手機
+        window.location.assign(`https://line.me/R/msg/text/?${document.querySelector('title').innerHTML}%0D%0A%0D%0A${document.querySelector('meta[property="og:description"]').content}%0D%0A%0D%0A${this.href}`);
+      } else {
+        window.open(`https://line.me/R/msg/text/?${document.querySelector('title').innerHTML}%0D%0A%0D%0A${document.querySelector('meta[property="og:description"]').content}%0D%0A%0D%0A`);
+      }
+      window.ga('newmedia.send', {
+        hitType: 'event',
+        eventCategory: 'Share',
+        eventAction: 'click',
+        eventLabel: `[${Utils.detectPlatform()}] [${document.querySelector('title').innerHTML}] [特製line icon] [Click_Line_top]`,
+      });
+    },
+    sendLogoGA() {
+      window.ga('newmedia.send', {
+        hitType: 'event',
+        eventCategory: 'logo',
+        eventAction: 'click',
+        eventLabel: `[${Utils.detectPlatform()}] [${document.querySelector('title').innerHTML}] [圓形logo點擊]`,
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.head-bar-container {
+  position: fixed;
+  z-index: 5000;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-image: linear-gradient(to top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.64));
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+  button {
+    background-color: transparent;
+    border: none;
+  }
+  .disabled {
+    pointer-events: none;
+    cursor: default;
+  }
+  .logo-wrapper {
+    position: relative;
+    width: 30px;
+    height: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 30px;
+    margin: 10px;
+    cursor: pointer;
+    i {
+      color: #f1f1f1;
+      transform: rotate(0deg);
+      &:hover {
+        transition: transform 288ms ease-in;
+        transform: rotate(16deg);
+      }
+    }
+  }
+  .share-button-container {
+    position: relative;
+    margin: 0 5px;
+    .share-button {
+      position: relative;
+      width: 30px;
+      height: 30px;
+      margin: 10px 5px;
+      img {
+        position: relative;
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+}
+</style>
---
+++ japan/src/components/cover/GradientConnection.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="gradient-connection">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GradientConnection'
+}
+</script>
+
+<style lang="scss" scoped>
+.gradient-connection {
+  position: relative;
+  width: 100%;
+  height: 50vh;
+  background-image: linear-gradient(to top, #000000, rgba(0, 0, 0, 0));
+  transform: translateY(2px);
+}
+</style>
